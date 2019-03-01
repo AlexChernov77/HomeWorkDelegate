6c303e4d04f6c5eff390934c9303cce9249e5e5f
Александр <achernov@aplana.com>
2019-03-01 21:51:57 +0300

    second commit

---
+++ BorshDelegate/DateManager.h
@@ -0,0 +1,17 @@
+//
+//  DateManager.h
+//  BorshDelegate
+//
+//  Created by Александр on 01.03.2019.
+//  Copyright © 2019 Sberbank. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DateManager : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ BorshDelegate/DateManager.m
@@ -0,0 +1,13 @@
+//
+//  DateManager.m
+//  BorshDelegate
+//
+//  Created by Александр on 01.03.2019.
+//  Copyright © 2019 Sberbank. All rights reserved.
+//
+
+#import "DateManager.h"
+
+@implementation DateManager
+
+@end
